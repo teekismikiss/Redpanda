a9a20f64f4f07009b8ba86e010a76b234a8ee544
Teeki <teekismikiss@gmail.com>
2026-04-07 10:44:33 +0200

    a

--- layout.php
+++ layout.php
@@ -7,6 +7,20 @@ $tel     = $datos['tel'];
 $movil   = $datos['movil'];
 $horario = $datos['horario'];
 
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+if (isset($_SESSION['usuario'])) {
+    if ($_SESSION['usuario'] === 'admin') {
+        echo ' <a href="logout.php" class="btnSave">LogOut</a>';
+        
+    }
+} else {
+    echo '<a href="creaUsu.php" class="btnSave" style="color: #ffffff; font-size: small;">Login</a>';
+}
+
+
 // Variables mínimas que debe definir cada página
 // $pageTitle  (opcional, para <title> o h1)
 // $pageView   (obligatoria, ruta de la vista dentro de /vistas)
